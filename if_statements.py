5178e9bc5830b87247a540a8796e2eb7a81c268a
payce06 <paycegrossman@gmail.com>
2024-12-07 20:43:27 -0500

    if-statements-py

--- if_statements.py
+++ if_statements.py
@@ -28,3 +28,32 @@ if x > 10:
         print("x is greater than or equal to 30")
 else:
     print("x is less than or equal to 10")
+
+    # 5. Using logical operators
+age = 25
+if age >= 18 and age <= 35:
+    print("You are eligible for the program")
+age = 70
+if age < 18 or age > 60:
+    print("You qualify for a discount")
+logged_in = False
+if not logged_in:
+    print("Please log in")
+
+# 6. Using `pass`
+x = 10
+if x > 5:
+    pass  # Do nothing for now
+else:
+    print("x is not greater than 5")
+
+# Practical examples
+
+# 7. Check if a number is positive, negative, or zero
+num = int(input("Enter a number: "))
+if num > 0:
+    print("The number is positive")
+elif num < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
