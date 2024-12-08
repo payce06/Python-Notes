294c6e582935cdea630d6d6b280c315a60d80190
payce06 <paycegrossman@gmail.com>
2024-12-07 19:39:36 -0500

    basic-py

--- basic.py
+++ basic.py
@@ -1,3 +1,38 @@
+# From float to int
+pi = 3.14
+int_pi = int(pi)  # Result: 3
+
+# From string to int
+num_str = "42"
+num = int(num_str)  # Result: 42
+
+# Invalid conversion
+invalid_str = "abc"
+# int(invalid_str)  # Raises ValueError
+
+print(int_pi, num)
+
+# From int to float
+x = 10
+float_x = float(x)  # Result: 10.0
+
+# From string to float
+num_str = "3.14"
+pi = float(num_str)  # Result: 3.14
+print(float_x, pi)
+
+# From int/float to string
+x = 10
+pi = 3.14
+str_x = str(x)  # Result: "10"
+str_pi = str(pi)  # Result: "3.14"
+
+# From boolean to string
+is_ready = True
+str_bool = str(is_ready)  # Result: "True"
+
+print(str_x, str_pi, str_bool)
+
 # From int to boolean
 x = 0
 bool_x = bool(x)  # Result: False
