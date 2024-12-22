d6cff46fd3387feb70fab96ba1e19ff85270902d
payce06 <paycegrossman@gmail.com>
2024-12-22 15:36:52 -0500

    set-py

--- set.py
+++ set.py
@@ -18,3 +18,22 @@ print("contains(Orange):", "Orange" in mySet)  # False
 # Set Length
 print("lengthOfSet:", len(mySet))  # Number of elements in the set
 print()
+
+# 2. Set Operations
+print("SetOperations:")
+setA = {1, 2, 3, 4}
+setB = {3, 4, 5, 6}
+
+# Union
+print("union:", setA.union(setB))  # {1, 2, 3, 4, 5, 6}
+
+# Intersection
+print("intersection:", setA.intersection(setB))  # {3, 4}
+
+# Difference
+print("difference(A-B):", setA.difference(setB))  # {1, 2}
+print("difference(B-A):", setB.difference(setA))  # {5, 6}
+
+# 3. Common Set Methods
+print("CommonSetMethods:")
+numbers = {1, 2, 3}
