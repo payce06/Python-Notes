032f076880c50bec6b99ba84eb7b01726162f1bf
payce06 <paycegrossman@gmail.com>
2024-12-22 18:42:11 -0500

    practice-problems3-py

--- practice-problems3.py
+++ practice-problems3.py
@@ -7,3 +7,14 @@ def findLength(array):
 
 # Example
 print(findLength([1, 2, 3, 4]))  # Output: 4
+
+
+# 2. Write a function to find the sum of all elements in an array.
+def sumOfElements(array):
+    total = 0
+    for num in array:
+        total += num
+    return total
+
+# Example
+print(sumOfElements([1, 2, 3, 4]))  # Output: 10
