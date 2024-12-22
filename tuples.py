1a4ba4e3bbbb404a9726513a1b68474ac84de19c
payce06 <paycegrossman@gmail.com>
2024-12-21 21:40:20 -0500

    tuples.py

--- tuples.py
+++ tuples.py
@@ -18,3 +18,21 @@ print("firstThreeElements:", nums[:3])  # Getting the first 3 values. (0, 1, 2)
 print("lastTwoElements:", nums[-2:])  # Getting the last two values. (4, 5)
 print("reversedTuple:", nums[::-1])  # Reversing a tuple. (5, 4, 3, 2, 1, 0)
 print()
+
+# 3. Tuple Methods
+print("TupleMethods:")
+numbers = (4, 2, 8, 6, 2)
+print("originalTuple:", numbers)
+
+# Count and Index
+countOf2 = numbers.count(2)
+print("countOf2:", countOf2)  # 2
+indexOf8 = numbers.index(8)
+print("indexOf8:", indexOf8)  # 2
+print()
+
+# 4. Iterating Over Tuples
+print("IteratingOverTuples:")
+fruits = ("Apple", "Banana", "Cherry")
+for fruit in fruits:
+    print(fruit)
