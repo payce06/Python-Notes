1e7f1105d35c07e63fcd14040106caaa79e59f2d
payce06 <paycegrossman@gmail.com>
2024-12-22 16:20:02 -0500

    practic-problems1-py

--- practice-problems1.py
+++ practice-problems1.py
@@ -20,3 +20,24 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n - 1)
+
+# Example
+print(factorial(5))  # Output: 120
+
+# 4. Write a function to check if a string is a palindrome.
+def isPalindrome(string):
+    return string == string[::-1]
+
+# Example
+print(isPalindrome("radar"))  # Output: True
+
+# 5. Write a function to calculate the nth Fibonacci number.
+def fibonacci(n):
+    if n <= 0:
+        return "Invalid input"
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
