1f658d6bd40b87813737798b19f1074bccdc81fd
payce06 <paycegrossman@gmail.com>
2024-12-28 20:25:12 -0500

    game-py

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+
+class Player:
+    def __init__(self, name, health, attack):
+        self.name = name
+        self.health = health
+        self.attack = attack
+
+    def is_alive(self):
+        return self.health > 0
