6eef8c20ccb9288550c4a738b143cfed7aa14e3e
payce06 <paycegrossman@gmail.com>
2025-01-11 21:56:56 -0500

    quiz-dictionary2-py

--- quiz-dictionary2.py
+++ quiz-dictionary2.py
@@ -55,7 +55,6 @@ def get_keys(obj):
 
 print(get_keys({"a": 1, "b": 2}))  # Output: ['a', 'b']
 
-
 # 7. Function to get the values of an object
 def get_values(obj):
     values = []
@@ -89,7 +88,6 @@ def delete_property(obj, key):
 
 print(delete_property({"a": 1, "b": 2}, "b"))  # Output: {'a': 1}
 
-
 # 11. Function to check if two objects are equal
 def are_objects_equal(obj1, obj2):
     if len(obj1) != len(obj2):
@@ -120,7 +118,6 @@ def sort_objects(arr, prop):
 
 print(sort_objects([{"a": 3}, {"a": 1}, {"a": 2}], "a"))  # Output: [{'a': 1}, {'a': 2}, {'a': 3}]
 
-
 # 14. Function to create an object from two arrays, one of keys and one of values
 def create_object(keys, values):
     obj = {}
@@ -147,3 +144,47 @@ def is_object_empty(obj):
 
 print(is_object_empty({}))  # Output: True
 
+# 17. Function that takes two objects and returns the common keys and their values
+def common_keys_values(obj1, obj2):
+    common = {}
+    for key in obj1:
+        if key in obj2 and obj1[key] == obj2[key]:
+            common[key] = obj1[key]
+    return common
+
+print(common_keys_values({"a": 1, "b": 2}, {"b": 2, "c": 3}))  # Output: {'b': 2}
+
+# 18. Function that finds the keys with the highest value in an object
+def keys_with_highest_value(obj):
+    max_value = None
+    for key in obj:
+        if max_value is None or obj[key] > max_value:
+            max_value = obj[key]
+    highest_keys = []
+    for key in obj:
+        if obj[key] == max_value:
+            highest_keys.append(key)
+    return highest_keys
+
+print(keys_with_highest_value({"a": 1, "b": 3, "c": 3}))  # Output: ['b', 'c']
+
+# 19. Function to convert an object to a query string
+def object_to_query_string(obj):
+    query_string = ""
+    for key, value in obj.items():
+        query_string += f"{key}={value}&"
+    return query_string.rstrip("&")
+
+print(object_to_query_string({"a": 1, "b": 2}))  # Output: 'a=1&b=2'
+
+# 20. Function that returns the nested value of an object given a key path
+def get_nested_value(obj, key_path):
+    keys = key_path.split(".")
+    for key in keys:
+        if isinstance(obj, dict) and key in obj:
+            obj = obj[key]
+        else:
+            return None
+    return obj
+
+print(get_nested_value({"a": {"b": {"c": 1}}}, "a.b.c"))  # Output: 1
